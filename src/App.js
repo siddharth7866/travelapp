d36331e74d68703aa83bbbeadc466c1888f3ab7c
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-08-29 14:39:56 +0530

    form done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Books", quantity: 4, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -13,15 +19,49 @@ function Logo() {
 }
 
 function Form() {
+  function sidSubmit(sid) {
+    // alert("submit clicked");
+    sid.preventDefault();
+    console.log(sid);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={sidSubmit}>
       <h3>What do you want for your travel?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((x) => (
+          <option value={x} key={x}>
+            {x}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
 function Packinglist() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
