32cae01380e69c4cd45f1793700dc5f0a9b3c022
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-08-29 12:13:49 +0530

    App file added

---
+++ src/App.js
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Sid />
+    </div>
+  );
+}
+
+function Hello() {
+  return (
+    <div>
+      <h1>Hello Siddharth</h1>
+    </div>
+  );
+}
+
+function Sid() {
+  return (
+    <div>
+      <h1>Hello Sid component</h1>
+    </div>
+  );
+}
+
+export default App;
